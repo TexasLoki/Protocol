25f93e05f0117ea2a745f9877fb47b4dfaea003b
Harry Fox <harry.fox.ic@icloud.com>
2014-08-23 12:24:35 +0100

    Code Reformat

--- src/main/java/org/pistonmc/protocol/v5/ProtocolStart.java
+++ src/main/java/org/pistonmc/protocol/v5/ProtocolStart.java
@@ -1,9 +1,5 @@
 package org.pistonmc.protocol.v5;
 
-import org.pistonmc.stickypiston.network.protocol.Protocol;
-
-import java.util.Map;
-
 public class ProtocolStart {
 
     public static void main(String[] args) {
