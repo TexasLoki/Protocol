279c6124ff077d8ee59ac48cb55641438cce6838
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 18:02:46 +0100

    Add PacketPlayOutCollectItem

---
+++ src/main/java/org/pistonmc/protocol/v5/play/server/PacketPlayOutCollectItem.java
@@ -0,0 +1,35 @@
+package org.pistonmc.protocol.v5.play.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.packet.OutgoingPacket;
+import org.pistonmc.protocol.packet.ProtocolState;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+
+import java.io.IOException;
+
+public class PacketPlayOutCollectItem extends OutgoingPacket {
+
+    private int entity;
+    private int collector;
+
+    public PacketPlayOutCollectItem(int entity, int collector) {
+        super(ProtocolState.PLAY, 0x0D);
+        this.entity = entity;
+        this.collector = collector;
+    }
+
+    public int getEntity() {
+        return entity;
+    }
+
+    public int getCollector() {
+        return collector;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        stream.writeInt(entity);
+        stream.writeInt(collector);
+    }
+
+}
