e9f2ab854da4c13de55f440d7554ae4fd926f1b2
jshh <jsh.github@gmail.com>
2014-08-26 21:00:15 +0100

    Add PacketPlayOutDisplayScoreboard

---
+++ src/main/java/org/pistonmc/protocol/v5/play/server/PacketPlayOutDisplayScoreboard.java
@@ -0,0 +1,35 @@
+package org.pistonmc.protocol.v5.play.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.packet.OutgoingPacket;
+import org.pistonmc.protocol.packet.ProtocolState;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+
+import java.io.IOException;
+
+public class PacketPlayOutDisplayScoreboard extends OutgoingPacket {
+
+	private byte position;
+	private String boardName;
+
+	public PacketPlayOutDisplayScoreboard(byte position, String boardName) {
+		super(ProtocolState.PLAY, 0x3D);
+		this.position = position;
+		this.boardName = boardName;
+	}
+
+	@Override
+	public void write(PacketOutputStream stream) throws PacketException, IOException {
+		stream.writeByte(position);
+		stream.writeString(boardName);
+	}
+
+	public byte getPosition() {
+		return position;
+	}
+
+	public String getBoardName() {
+		return boardName;
+	}
+
+}
