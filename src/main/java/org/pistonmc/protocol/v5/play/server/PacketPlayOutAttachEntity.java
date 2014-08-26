3a5fdcb9770f8beb05105a0947f2550ae0d12792
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 20:45:42 +0100

    Add PacketPlayOutAttachEntity

---
+++ src/main/java/org/pistonmc/protocol/v5/play/server/PacketPlayOutAttachEntity.java
@@ -0,0 +1,35 @@
+package org.pistonmc.protocol.v5.play.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+import org.pistonmc.protocol.v5.play.server.groups.PacketPlayOutEntity;
+
+import java.io.IOException;
+
+public class PacketPlayOutAttachEntity extends PacketPlayOutEntity {
+
+    private int vehicle;
+    private boolean leash;
+
+    public PacketPlayOutAttachEntity(int entity, int vehicle, boolean leash) {
+        super(0x1A, entity);
+        this.vehicle = vehicle;
+        this.leash = leash;
+    }
+
+    public int getVehicle() {
+        return vehicle;
+    }
+
+    public boolean isLeash() {
+        return leash;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        super.write(stream);
+        stream.writeInt(vehicle);
+        stream.writeBoolean(leash);
+    }
+
+}
