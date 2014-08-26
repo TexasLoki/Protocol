5e0896d0a073853a3cccfe98c79f7b25e68f3f9b
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 18:16:09 +0100

    Add PacketPlayOutSpawnExperienceOrb

---
+++ src/main/java/org/pistonmc/protocol/v5/play/server/PacketPlayOutSpawnExperienceOrb.java
@@ -0,0 +1,49 @@
+package org.pistonmc.protocol.v5.play.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+import org.pistonmc.protocol.v5.play.server.groups.PacketPlayOutEntity;
+
+import java.io.IOException;
+
+public class PacketPlayOutSpawnExperienceOrb extends PacketPlayOutEntity {
+
+    private int x;
+    private int y;
+    private int z;
+    private short count;
+
+    public PacketPlayOutSpawnExperienceOrb(int entity, int x, int y, int z, short count) {
+        super(0x11, entity);
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.count = count;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public short getCount() {
+        return count;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        super.write(stream);
+        stream.writeInt(x);
+        stream.writeInt(y);
+        stream.writeInt(z);
+        stream.writeShort(count);
+    }
+
+}
