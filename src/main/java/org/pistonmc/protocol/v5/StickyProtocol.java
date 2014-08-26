5547f1f40b99c84533ce7e36a928f773de52de14
Harry Fox <harry.fox.ic@icloud.com>
2014-08-26 14:30:34 +0100

    Updates to Protocol and work on Packets

--- src/main/java/org/pistonmc/protocol/v5/StickyProtocol.java
+++ src/main/java/org/pistonmc/protocol/v5/StickyProtocol.java
@@ -1,21 +1,33 @@
 package org.pistonmc.protocol.v5;
 
+import org.pistonmc.exception.protocol.packet.PacketException;
 import org.pistonmc.plugin.protocol.Protocol;
-import org.pistonmc.protocol.Client;
+import org.pistonmc.protocol.PlayerConnection;
+import org.pistonmc.protocol.packet.Packet;
 
 public class StickyProtocol extends Protocol {
 
     public StickyProtocol() {
-        this(null);
+        super(5);
     }
 
-    public StickyProtocol(Client client) {
-        super(client);
+    public StickyProtocol(int version, PlayerConnection connection) {
+        super(version, connection);
     }
 
     @Override
-    public Protocol create(Client client) {
-        return new StickyProtocol(client);
+    public void onEnable() {
+        // add packets here using add(Packet)
+    }
+
+    @Override
+    public void handle(Packet packet) throws PacketException {
+
+    }
+
+    @Override
+    public Protocol create(PlayerConnection connection) {
+        return new StickyProtocol(version, connection);
     }
 
 }
