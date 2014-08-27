534aab0414181cd8e73a6e48349756eb2d8e7c7a
Kailan Blanks | Komp <kailan@komputerking.net>
2014-08-27 21:08:06 +0100

    Add PacketPlayOutSoundEffect

---
+++ src/main/java/org/pistonmc/protocol/v5/play/server/PacketPlayOutSoundEffect.java
@@ -0,0 +1,39 @@
+package org.pistonmc.protocol.v5.play.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.packet.OutgoingPacket;
+import org.pistonmc.protocol.packet.ProtocolState;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+
+import java.io.IOException;
+
+public class PacketPlayOutSoundEffect extends OutgoingPacket {
+
+    private int sound;
+    private int x;
+    private int y;
+    private int z;
+    private float volume;
+    private byte pitch;
+
+    public PacketPlayOutSoundEffect(int sound, int x, byte y, int z, float volume, byte pitch) {
+        super(ProtocolState.PLAY, 0x29);
+        this.sound = sound;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.volume = volume;
+        this.pitch = pitch;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        stream.writeInt(sound);
+        stream.writeInt(x);
+        stream.writeInt(y);
+        stream.writeInt(z);
+        stream.writeFloat(volume);
+        stream.writeUnsignedByte(pitch);
+    }
+
+}
