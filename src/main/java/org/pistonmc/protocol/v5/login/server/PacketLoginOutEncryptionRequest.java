d8e2ca1c69b86c09de622a65829a87277a7fbd5c
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 10:37:28 +0100

    Add PacketLoginOutEncryptionRequest

---
+++ src/main/java/org/pistonmc/protocol/v5/login/server/PacketLoginOutEncryptionRequest.java
@@ -0,0 +1,43 @@
+package org.pistonmc.protocol.v5.login.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.packet.OutgoingPacket;
+import org.pistonmc.protocol.packet.ProtocolState;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class PacketLoginOutEncryptionRequest extends OutgoingPacket {
+
+    private String sessionId;
+    private byte[] publicKey;
+    private byte[] verifyToken;
+
+    public PacketLoginOutEncryptionRequest(String sessionId, byte[] publicKey, byte[] verifyToken) {
+        super(ProtocolState.LOGIN, 0x01);
+        this.sessionId = sessionId;
+        this.publicKey = publicKey;
+        this.verifyToken = verifyToken;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public byte[] getPublicKey() {
+        return publicKey;
+    }
+
+    public byte[] getVerifyToken() {
+        return verifyToken;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        stream.writeString(sessionId);
+        stream.writeByteArray(publicKey);
+        stream.writeByteArray(verifyToken);
+    }
+
+}
