5021b9e81fea6a85ae52729d07050878691821b1
Harry Fox <harry.fox.ic@icloud.com>
2014-08-28 10:37:22 +0100

    Add PacketLoginOutEncryptionRequest

---
+++ src/main/java/org/pistonmc/protocol/v5/login/server/PacketLoginOutLoginSuccess.java
@@ -0,0 +1,37 @@
+package org.pistonmc.protocol.v5.login.server;
+
+import org.pistonmc.exception.protocol.packet.PacketException;
+import org.pistonmc.protocol.packet.OutgoingPacket;
+import org.pistonmc.protocol.packet.ProtocolState;
+import org.pistonmc.protocol.stream.PacketOutputStream;
+import org.pistonmc.util.ChatFormatter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class PacketLoginOutLoginSuccess extends OutgoingPacket {
+
+    private UUID uuid;
+    private String username;
+
+    public PacketLoginOutLoginSuccess(UUID uuid, String username) {
+        super(ProtocolState.LOGIN, 0x02);
+        this.uuid = uuid;
+        this.username = username;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void write(PacketOutputStream stream) throws PacketException, IOException {
+        stream.writeString(uuid.toString());
+        stream.writeString(username);
+    }
+
+}
